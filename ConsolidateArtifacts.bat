db970343809f586e7ac2c646165f96912dbc8e21
AntoineConffx <antoine@conffx.com>
2017-06-30 14:14:05 -0700

    Found a bug in Consolidate Artifacts related to OUT_DIR usage and fixed the error line to recommended usage

--- ConsolidateArtifacts.bat
+++ ConsolidateArtifacts.bat
@@ -3,19 +3,19 @@ setlocal EnableDelayedExpansion
 :: Libraries from local folder
 @SETLOCAL
 @ECHO off
-call "%VS140COMNTOOLS%..\..\VC\bin\vcvars32.bat"
+
 
 set "OUT_DIR=./Artifacts"
 set "CURR_DIR=."
 
 ::remove original artifacts (if any)
-if exist "OUT_DIR" rmdir "%OUT_DIR%" /s /q
+if exist "%OUT_DIR%" rmdir /s /q "%OUT_DIR%"
 ::create artifacts folder
 mkdir "%OUT_DIR%"
 
 ::go to json11 folder and move json.hpp into artifact folder
 xcopy  "src\*.hpp" "%OUT_DIR%" /y/s/q/i
-if !errorlevel! neq 0 exit /b !errorlevel!
+if errorlevel 1 exit /b %errorlevel%
 
 exit /b 0
 
